b848379fe62d89b3f381e72945579c512b8f4259
suresh <suresh.ch471@gmail.com>
2016-12-05 02:26:12 -0700

    added styling for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
    before_action :authenticate_user!
+
+
+   private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  helper_method :current_user
 end
